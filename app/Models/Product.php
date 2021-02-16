6ab36e4ea70d51b07653e020b92eb63715681fc3
Paria <pariazeynali1999@gmail.com>
2021-02-16 23:54:15 +0330

    adding create product controller

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-
-    protected $filable =['productname','company','price','count','kind_id','type_id','skintypeid','pic'];
     use HasFactory;
+
+    protected $table = 'product';
+
+    protected $fillable =['productname','company','price','count','kind_id','type_id','skintypeid','pic'];
+
 }
