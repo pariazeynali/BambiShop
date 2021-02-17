832348f48f64a76aeaf7d6343b9822fb78346a5a
Paria <pariazeynali1999@gmail.com>
2021-02-17 17:23:22 +0330

    edit database

--- database/migrations/2021_02_10_002851_create_carts_table.php
+++ database/migrations/2021_02_10_002851_create_carts_table.php
@@ -13,9 +13,9 @@ class CreateCartTable extends Migration
      */
     public function up()
     {
-        Schema::create('cart', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            //$table->;
+            //$table->binary('cart');
         });
     }
 
---
+++ database/migrations/2021_02_17_002852_create_customers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('national_id',10)->unique();
+            $table->string('phone_number',11)->unique();
+            $table->string('province');
+            $table->string('city');
+            $table->string('postcode',10)->unique();
+            $table->longText('address');
+
+
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
