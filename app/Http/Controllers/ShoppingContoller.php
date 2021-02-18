a28eba571d2054bdbcc1d5aeb01145287631b1ac
Paria <pariazeynali1999@gmail.com>
2021-02-19 03:12:54 +0330

    add api of adding to carts

--- app/Http/Controllers/ShoppingContoller.php
+++ app/Http/Controllers/ShoppingContoller.php
@@ -10,21 +10,17 @@ use Illuminate\Support\Facades\DB;
 
 class ShoppingContoller extends Controller
 {
-    public function addToCart(Request $request,Product $product)
+    public function addToCart(Request $request)
     {
         $validated = $request->validate([
-            'id' => 'number|exists:products,id',
+            'product_id' => 'number|exists:products,id',
         ]);
 
-        if ($product){
-            Cart::create([
-                'product_id' => $product->id,
-                'user_id' => $request->user()->id,
-            ]);
-            return response()->json(['flag'=>true,'message'=>'با موفقیت در سبد خرید اضافه شد']);
-        }else{
-            return response()->json(['flag'=>false,'message'=>'محصول مورد نظر یافت نشد']);
-        }
+        Cart::create([
+            'product_id' => $request->product_id,
+            'user_id' => $request->user()->id,
+        ]);
+        return response()->json(['flag'=>true,'message'=>'با موفقیت در سبد خرید اضافه شد']);
 
     }
 
