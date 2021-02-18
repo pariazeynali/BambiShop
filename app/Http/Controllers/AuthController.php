f2de88fab20341c7f271a6496d1776deabcfc592
Paria <pariazeynali1999@gmail.com>
2021-02-18 20:53:32 +0330

    adding api token to users table

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,51 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Dotenv\Validator;
+use http\Client\Curl\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Expr\Array_;
+use function GuzzleHttp\Promise\all;
 
 class AuthController extends Controller
 {
 
-    protected function login_validate(Request $request){
-
-        $this->validate($request,[
-            $this->username() => 'required|string',
-            'password' => 'required|string',
-        ]);
-    }
-    public function login(Request $request)
-    {
-        $this->login_validate($request);
-
-        $user=DB::table('users')->where('username','=' , $request->username)->get()->first();
-        if(!$user) {
-            Session::flash('user.login', 'user not found');
-            return redirect()->route('login');
-        }
-
-        $credentials = $request->only('username', 'password');
-
-        if(Auth::attempt($credentials)) {
-            // Auth::login($user, true);
-            return redirect()->intended('dashboard');
-        } else {
-            Session::flash('user.login', 'invalid password');
-            return redirect()->route('login');
-        }
-
-
-    }
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    public function logout(Request $request){
-        $this->guard()->login();
-        $request->session()->invalidate();
-        return redirect('/');
-
-    }
 }
