573f0879320c967f6a33a20ebdccdde8210b9ebc
Paria <pariazeynali1999@gmail.com>
2021-02-18 21:30:24 +0330

    creating authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return response()->json(['message' => 'شما هنوز وارد نشدید']);
+            return 'شما هنوز وارد نشدید';
         }
     }
 }
