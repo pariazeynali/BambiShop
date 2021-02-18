9dbdae1a656e4e9e47feaf7420ce591d18e68a1b
Paria <pariazeynali1999@gmail.com>
2021-02-18 22:02:00 +0330

    creating authentication

--- database/migrations/2021_02_18_182310_add_datetime_tables.php
+++ database/migrations/2021_02_18_182310_add_datetime_tables.php
@@ -14,29 +14,7 @@ class AddDatetimeTables extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamps()->nullable();
-        });
-        Schema::table('kind', function (Blueprint $table) {
-            $table->timestamps()->nullable();
-        });
-        Schema::table('orders', function (Blueprint $table) {
-            $table->timestamps()->nullable();
-        });
-        Schema::table('customers', function (Blueprint $table) {
-            $table->timestamps()->nullable();
-        });
-        Schema::table('skintype', function (Blueprint $table) {
-            $table->timestamps()->nullable();
-        });
-
-        Schema::table('products', function (Blueprint $table) {
-            $table->timestamps()->nullable();
-        });
-        Schema::table('type', function (Blueprint $table) {
-            $table->timestamps()->nullable();
-        });
-        Schema::table('carts', function (Blueprint $table) {
-            $table->timestamps()->nullable();
+            $table->timestamps();
         });
     }
 
