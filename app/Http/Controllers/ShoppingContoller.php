40b25b44838d1e7a63bcfc008f16870a7732cd25
Paria <pariazeynali1999@gmail.com>
2021-02-20 15:42:46 +0330

    edit logout

--- app/Http/Controllers/ShoppingContoller.php
+++ app/Http/Controllers/ShoppingContoller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Cart;
 use App\Models\Product;
-use http\Client\Curl\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
