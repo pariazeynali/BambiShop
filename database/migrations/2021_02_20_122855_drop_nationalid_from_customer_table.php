fda7bc07cb950131f2b023d4017428bfb70cea65
Paria <pariazeynali1999@gmail.com>
2021-02-20 16:03:49 +0330

    edit logout

--- database/migrations/2021_02_20_122855_drop_nationalid_from_customer_table.php
+++ database/migrations/2021_02_20_122855_drop_nationalid_from_customer_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DropNationalidFromCustomersTable extends Migration
+class DropNationalidFromCustomerTable extends Migration
 {
     /**
      * Run the migrations.
