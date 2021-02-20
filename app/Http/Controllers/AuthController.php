d6ed168b75796ffd56392006f7b8449e34cd7a20
Paria <pariazeynali1999@gmail.com>
2021-02-20 13:05:13 +0330

    add flag for sing in

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
             ]
         );
 
-        return response()->json(['token' => $token]);
+        return response(['flag'=>true,'token'=>$token, 'username'=>$request->username],200);
     }
 
     public function signIn(Request $request)
