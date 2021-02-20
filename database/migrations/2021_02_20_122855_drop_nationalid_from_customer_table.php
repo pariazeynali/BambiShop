9fde07a89c1c39bc670c0787515e53258fe65a44
Paria <pariazeynali1999@gmail.com>
2021-02-20 16:08:19 +0330

    edit logout

--- database/migrations/2021_02_20_122855_drop_nationalid_from_customer_table.php
+++ database/migrations/2021_02_20_122855_drop_nationalid_from_customer_table.php
@@ -14,7 +14,7 @@ class DropNationalidFromCustomerTable extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->dropColumn('customer_id');
+            $table->dropColumn('national_id');
         });
     }
 
